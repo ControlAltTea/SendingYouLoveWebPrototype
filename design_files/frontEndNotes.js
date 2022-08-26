052418c42634de244ddcce0fe50e9d1926f1fbd2
Tea <controlalttea@gmail.com>
2022-08-26 00:13:29 -0700

    meet the team and user dashboard mockups

---
+++ design_files/frontEndNotes.js
@@ -0,0 +1,10 @@
+// COLORS
+    // page background - tan #fcfae9
+    // text color: #537556 (slighly lighter hunter green)
+    // Bodies of text - tan adjacent backgrounds
+        // tans for sections should be consistent but generally a darker shade of tan
+        // Meet the team uses #fbe3c4 on bodies of text
+    // banners (or navbars) have a slight drop shadow 
+    // font style - Champagne
+    // user icons found in PNGs
+    // official color palette in the palettes folder ("01")
