bebe9be62c5e926fbbad333a117f533dc0c6e981
Tea <controlalttea@gmail.com>
2022-09-06 14:39:09 -0700

    authentication WAS broken but now works. multiple users able to signup.

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -9,4 +9,8 @@ router.get("/dashboard", authController.getDashboard);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
 
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+router.get("/logout", authController.logout);
+
 module.exports = router;
